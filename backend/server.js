aff6840276f5ea98cb8a55b3d3be4e169f556b64
Danish Siddiqui <danbot555@gmail.com>
2024-11-22 18:02:56 -0500

    avg vol 3month + 10 day and vol ratios in finance api now

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors'); // Optional
+const yahooFinance = require('yahoo-finance2').default;
+
+
 
 const app = express();
 
 // Enable CORS (if needed)
 app.use(cors());
+
+
+async function getAvgVolume(symbol) {
+    try {
+        yahooFinance.suppressNotices['yahooSurvey'];
+        const result = await yahooFinance.quote(symbol);
+        return({
+            avgVol3Month: result.averageDailyVolume3Month,
+            avgVol10Day: result.averageDailyVolume10Day
+        });
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+
 
 // Backend API to fetch data from Yahoo Finance
 app.get('/api/finance', async (req, res) => {
@@ -22,23 +42,17 @@ app.get('/api/finance', async (req, res) => {
 
     const yahooData = response.data.chart.result[0];
     const volDay = yahooData.indicators.quote[0].volume
-
-    console.log(volDay);
-
-    const totalVol = (Array.isArray(volDay) ? volDay : []).reduce((accumulator, currentValue) => {
-        currentValue = currentValue || 0; // Default to 0 if falsy
-        return accumulator + currentValue;
-      }, 0);
+    const avgVol = await getAvgVolume(queries.ticker);
       
 
     const transformedData = {
         Symbol: yahooData.meta.symbol,
         Name: yahooData.meta.longName,
-        avgVol: yahooData.meta.regularMarketVolume,
-        totalVol: totalVol,
-        volRatio: totalVol/yahooData.meta.regularMarketVolume
-
-
+        avgVol3Month: avgVol.avgVol3Month,
+        avgVol10Day: avgVol.avgVol10Day,
+        volToday: yahooData.meta.regularMarketVolume,
+        volRatio3Month: yahooData.meta.regularMarketVolume/avgVol.avgVol3Month,
+        volRatio10Day: yahooData.meta.regularMarketVolume/avgVol.avgVol10Day
     }
     console.log(transformedData)
     // Send the fetched data to the frontend
